8e6ccd6773b11394aa7600c7b01d85479b48fa0f
miznos06 <miznos06@gmail.com>
2023-02-11 16:51:00 +0900

    test削除

--- test/test3-data1.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $data[] = array("area"=>1, "name"=>"バングラデシュ", "flag"=>"bangladesh.gif");
-    $data[] = array("area"=>4, "name"=>"カメルーン", "flag"=>"cameroon.gif");
-    $data[] = array("area"=>1, "name"=>"日本", "flag"=>"japan.gif");
-    $data[] = array("area"=>1, "name"=>"インドネシア", "flag"=>"indonesia.gif");
-    $data[] = array("area"=>3, "name"=>"アイルランド", "flag"=>"ireland.gif");
-    $data[] = array("area"=>2, "name"=>"ペルー", "flag"=>"peru.gif");
-?>
