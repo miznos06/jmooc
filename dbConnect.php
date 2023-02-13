a80f501c87fd27416399875c4799ae965b84dfc2
miznos06 <64647841+miznos06@users.noreply.github.com>
2023-02-13 16:01:45 +0900

    コメント追加

--- dbConnect.php
+++ dbConnect.php
@@ -1,11 +1,14 @@
 <?php
+    //ログインに必要な情報を変数に格納
     $DBSERVER = "localhost";
     $DBUSER = "root";
     $DBPASSWORD = "";
     $DBNAME = "stock";
 
+    //SQLに接続
     $conn = mysqli_connect($DBSERVER, $DBUSER, $DBPASSWORD, $DBNAME);
 
+    //接続エラー時のメッセージとexit
     if(mysqli_connect_error())
     {
         echo mysqli_connect_error();
